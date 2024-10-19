3f91e8134990dbc0e7561f98f82b9043ec84166f
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2024-10-19 08:37:17 +0530

    startup sql script update

--- mysql/startup_sql/init_database.sql
+++ mysql/startup_sql/init_database.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE IF NOT EXISTS Company;
+CREATE DATABASE IF NOT EXISTS expense_tracker;
 
-CREATE TABLE IF NOT exists Company.Employees (
-	employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-	first_name  VARCHAR(255) NOT NULL,
-	last_name   VARCHAR(255) NOT NULL,
-	email       VARCHAR(255) NOT NULL,
-	phone       VARCHAR(50) NOT NULL ,
-	hire_date   DATE NOT NULL,
-	manager_id  INTEGER REFERENCES Employees(employee_id),
-	job_title   VARCHAR(255) NOT NULL
+USE expense_tracker;
+
+CREATE TABLE IF NOT exists expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(100),
+    date DATE NOT NULL
 );
 
